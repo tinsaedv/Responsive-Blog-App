91f26fa8583fead8d75960b5f45baf89cb94b530
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2023-09-20 16:16:10 +0300

    changed the static serving folder to dist and added helmet config

--- server/src/app.js
+++ server/src/app.js
@@ -28,7 +28,16 @@ app.use(
   })
 );
 
-app.use(helmet());
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        'img-src': ["'self'", 'res.cloudinary.com', 'data:'],
+      },
+    },
+  })
+);
 
 app.use(express.json({ limit: '25mb' }));
 app.use(express.urlencoded({ limit: '25mb' }));
@@ -80,10 +89,9 @@ app.use('/api/comments', CommentRouter);
 app.use('/api', UploadImageRouter);
 app.use('/auth', OauthRouter);
 
-app.use(express.static(path.join(__dirname, '..', 'public')));
-
+app.use(express.static(path.join(__dirname, '../dist')));
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
 module.exports = app;
