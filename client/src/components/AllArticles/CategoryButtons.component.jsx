2691d3df70a2fcaa256a66fcb740876adba2b44c
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2023-09-20 15:54:51 +0300

    removed unused import files

--- client/src/components/AllArticles/CategoryButtons.component.jsx
+++ client/src/components/AllArticles/CategoryButtons.component.jsx
@@ -1,27 +1,17 @@
-import { useCallback } from 'react';
 import { useArticleStore } from '../../App/useArticleStore';
-import Articles from '../Home/HomeArticles.component';
+
 import { memo } from 'react';
 
 const CategoryButtons = memo(({ menuItems, filteredItems, setItems }) => {
   const {
-    setArticles,
-    allArticles,
     articles,
-    setSearchQuery,
-    searchQuery,
-    getArticles,
-  } = useArticleStore((state) => ({
-    setArticles: state.setArticles,
-    allArticles: state.allArticles,
-    articles: state.articles,
-    setSearchQuery: state.setSearchQuery,
-    searchQuery: state.searchQuery,
-    getArticles: state.getArticles,
-  }));
 
-  console.log(menuItems);
-  console.log('searchQuery', searchQuery);
+    searchQuery,
+  } = useArticleStore((state) => ({
+    articles: state.articles,
+
+    searchQuery: state.searchQuery,
+  }));
 
   // const handleOnclick = useCallback(
   //   (e) => {
