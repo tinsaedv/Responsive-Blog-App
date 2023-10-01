f724285a9426577abbf4992d2486fe0a0f30bb33
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2023-10-01 14:46:10 +0300

    changed require to import

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  plugins: [require('flowbite/plugin')],
+  plugins: [import('flowbite/plugin')],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
@@ -9,5 +10,6 @@ export default {
         Roboto: `'Roboto', sans-serif`,
       },
     },
+  
   },
 };
