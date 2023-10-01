6ea3ae8f0555696a33cd411649dacf116acc566a
Tinsae Yohannes <tinsaeyohannes@gmail.com>
2023-10-01 22:21:04 +0300

    Updated user model schema to include new fields for followers count, following count, articles count, and verified status, and to remove the stats array and its properties. Added a new field for Google authentication.

--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -18,28 +18,29 @@ const userSchema = new mongoose.Schema(
     },
     password: {
       type: String,
-      required: true,
-    },
-    stats: [
-      {
-        followersCount: {
-          type: Number,
-          default: 0,
-        },
-        followingCount: {
-          type: Number,
-          default: 0,
-        },
-        articlesCount: {
-          type: Number,
-          default: 0,
-        },
-        verified: {
-          type: Boolean,
-          default: false,
-        },
+      required: function () {
+        return !this.google;
       },
-    ],
+    },
+    stats: {
+      followersCount: {
+        type: Number,
+        default: 0,
+      },
+      followingCount: {
+        type: Number,
+        default: 0,
+      },
+      articlesCount: {
+        type: Number,
+        default: 0,
+      },
+      verified: {
+        type: Boolean,
+        default: false,
+      },
+    },
+
     socials: {
       facebook: String,
       instagram: String,
@@ -50,6 +51,10 @@ const userSchema = new mongoose.Schema(
 
     likedArticles: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'Article' }],
     followers: [{ type: mongoose.SchemaTypes.ObjectId, ref: 'User' }],
+    google: {
+      type: Boolean,
+      default: false,
+    },
   },
 
   { timestamps: true }
